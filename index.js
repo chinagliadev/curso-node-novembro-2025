d2f696bce5047de55e314af00271034998e478bb
Chinaglia <156460261+chinagliadev@users.noreply.github.com>
2025-11-07 22:26:32 -0300

    entendo rotas(endpoint)

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("ola")
+const express = require('express') 
+const app = express() 
+
+app.get('/', function(req, res){ 
+    res.send('aaa world')
+})
+
+app.get('/pastel', (req, res) => {res.send('seu paster chegou bb')})
+
+
+app.listen(3000) 
